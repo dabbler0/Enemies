07c339bcedcdcc8444e9cc9d42d828b49c7c7949
dabbler0 <dab1998@gmail.com>
2013-02-21 19:59:50 -0500

    Not compilable, do not revert to this version, backup only.

--- enemies.cc
+++ enemies.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-using namepsace std;
+using namespace std;
 
 const double TERRAIN_NUMBER = 0.5;
 
@@ -12,7 +12,7 @@ class Bot {
       Bot (char* args[]) {
          //Set up pipes:
          int in_pipe[2];
-         int out_pipe[2]:
+         int out_pipe[2];
          int error_pipe[2];
 
          pipe(in_pipe);
@@ -75,10 +75,20 @@ class Bot {
 }
 
 struct Node {
+   static int last_id = 0;
    int id;
    int color;
    double coords[2];
    double power;
+   Node () {
+      coords[0] = 0;
+      coords[1] = 0;
+      id = last_id;
+      power = 1;
+      color = -1;
+      
+      last_id += 1;
+   }
    Node (double x, double y, double i) {
       coords[0] = x;
       coords[1] = y;
@@ -91,19 +101,19 @@ struct Node {
 class Board {
    private:
       Node* nodes;
-      double** paths;
+      double** contributions;
       int l;
    public:
       Board (int n, int w, int h) {
          //Set up an array for temporary storage:
          nodes = new Node[2*n];
-         paths = new double[2*n][2*n];
-
-         //Fill the path lengths with negative ones, signifying "no such path."
+         contributions = new double[2*n][2*n];
+         
+         //Set up the contribution array:
          for (int i = 0; i < 2*n; i += 1) {
             for (int x = 0; x < 2*n; x += 1) {
-               if (x == 1) paths[i][x] = 0;
-               else paths[i][x] = -1;
+               if (x == 1) [i][x] = 1;
+               else [i][x] = 0;
             }
          }
 
@@ -111,22 +121,23 @@ class Board {
          l = 2*n;
          
          //"Home" nodes:
-         Node red_home (0, 0);
-         temp[0] = red_home;
-         Node blue_home (w, 0);
-         temp[1] = blue_home;
+         temp[0].color = 1; //Red home
+         temp[1].coords[0] = w;
+         temp[1].color = 2; //Blue home
 
          //Randomly generated nodes:
          for (int i = 2; i < n; i += 2) {
             //Randomly generate the coordinates of the new node:
             double x = w * rand() / RAND_MAX;
             double y = h * rand() / RAND_MAX;
+            
+            //Move node i into position:
+            nodes[i].coords[0] = x;
+            nodes[i].coords[1] = y;
 
-            //Each node has a reflected node, to make a fair board:
-            Node k (x, y, i);
-            Node l (w - x, y, i + 1)
-            nodes[i] = k;
-            nodes[i + 1] = l;
+            //Each node has a reflected pair, to make a fair board:
+            nodes[i + 1].coords[0] = w - x;
+            nodes[i + 1].coords[1] = y;
          }
       }
 
@@ -149,14 +160,27 @@ class Board {
             double terrain_factor = pow(TERRAIN_NUMBER, distance(a,b));
 
             if (a.power * terrain_factor > b.power) {
-               //If the attack is successful, change b's power and color:
+               //If the attack is successfuly, remove all enemy paths involving b:
+               for (int i = 0; i < l; i += 1) {
+                  if (nodes[i].color == b.color) {
+                     for (int x = 0; x < l; x += 1) {
+                        if (nodes[x].color == b.color) {
+                           contributions[i][x] -= contributions[i][b] * contributions[b][x];
+                        }
+                     }
+                  }
+               }
+               
+               //Change b's power and color:
                b.power = a.power * terrain_factor;
                b.color = a.color;
-            
-               //Then go to every owned node and increase its power:
+
+                          
+               //Then go to every node owned by (a) and increase its power:
                for (int i = 0; i < l; i += 1) {
                   if (nodes[i].color == a.color) {
-                     nodes[i].power += pow(TERRAIN_NUMBER, paths[i][a]) * terrain_factor;
+                     nodes[i].power += pow(TERRAIN_NUMBER, contributions[i][a]) * terrain_factor;
+                     contributions[i][x] = contributions[i][a] * terrain_factor;
                   }
                }
                return true;
@@ -166,25 +190,49 @@ class Board {
                return false;
             }
          }
+
          else {
-            //If a and b are allied, shorten paths and increase powers:
-            double distance = distance(a,b);
+            //If a and b are allied, record contributions and increase powers:
+            double difficulty = pow(TERRAIN_NUMBER, distance(a,b));
             for (int i = 0; i < l; i += 1) {
                if (nodes[i].color == a.color) {
                   for (int x = 0; x < l; x += 1) {
-                     if (nodes[x].color == a.color && paths[i][x] > paths[i][a] + distance + paths[b][x]) {
-                        
-                        //Unlink the old path:
-                        nodes[i].power -= pow(TERRAIN_NUMBER, paths[i][x]);
-                        nodes[x].power -= pow(TERRAIN_NUMBER, paths[i][x]);
-                        
-                        //Link up the new path:
-
-                        paths[i][x] = paths[i][a] + distance + paths[b][x]; 
+                     if (nodes[x].color == a.color) {
+                        //Recompute contributions:
+                        double new_contribution = contributions[i][a] * difficulty * contributions[b][x];
+                        contributions[i][x] += new_contribution;
+                        nodes[x].power += new_contribution;
                      }
                   }
                }
             }
          }
+         //If we get here, we are done!
+         return true;
       }
+
+      string fullDescription() {
+         //A description of the entire board for delivery at game start:
+         stringstream s;
+         for (int i = 0; i < l; i += 1) {
+            s << nodes[i].coords[0] << ' ' << nodes[i].coords[1] << ' ' << nodes[i].color << ' ' << nodes[i].power << ' '
+         }
+         return s.str();
+      }
+
+      string boardState() {
+         //A description of all the nodes' powers for delivery after every move:
+         stringstream s;
+         for (int i = 0; i < l; i += 1) {
+            s << nodes[i].power << ' ';
+         }
+         return s.str();
+      }
+}
+
+int main() {
+   //Currently, just a test of the board:
+   Board test (30);
+   cout << test.fullDescription() << endl;
+   return 0;
 }
